f8af5ee8fdfc4dbfcbdff66a6b03de1f7bf2f1ff
JudeKratzer <tdujgamer@gmail.com>
2018-03-30 13:59:47 -0700

    Update script.js

--- heat/script.js
+++ heat/script.js
@@ -72,14 +72,6 @@ function updateTemp(newTemp) {
   console.log(bcolor)
   $('body').css('background', bcol);
 }
-input.addEventListener("keyup", function(event) {
-  // Cancel the default action, if needed
-  event.preventDefault();
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Trigger the button element with a click
-    document.getElementById("#zip-button").click();
-  }
 });
 $( function() {
   $("#zip-button").button();
